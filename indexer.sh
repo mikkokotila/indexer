9928fba5a7418f1699bc80359f7fdc3424bb03aa
mikkokotila <mailme@mikkokotila.com>
2016-02-06 12:52:46 +0000

    Update indexer.sh

--- indexer.sh
+++ indexer.sh
@@ -1,9 +1,19 @@
+#!/bin/bash
 
-        # goes once in to the main file and creates temp file based on key1
-        # number of keys at this stage reduce time taken for calculations 
-        # but increase the time to build the index.
+# to start you need a large dataset where the first column data can
+# be used as a key for the index. 
 
-#	SOURCEFILE=(indexer.input)
+# there are two ways to control the compute time, one is the key1 and 
+# the other is key2. Roughly speaking: 
+#						key1	key2	
+# very big file with many things to do		>50	>1500
+# very big file but not so many things to do	>50	>500
+# smaller file with many things to do		>20	>500
+
+# KEY1SIZE=()
+# KEY2SIZE=()
+# SOURCEFILE=()
+
 	cat indexer.input | colrm 3 | grep -e ^"[a-zA-Z0-9]" | sort -u > indexer.keyspace
 	cat indexer.keyspace | colrm 3 > indexer.key1
 
